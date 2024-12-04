445afcd4f9b42f5f7bd1820f035f3f465989d654
pedrocrc <pedrocrc@gmail.com>
2024-12-04 10:48:51 -0300

    Use pipe delimiter in sed command for better readability in cluster-up.sh

--- cluster-up.sh
+++ cluster-up.sh
@@ -1,4 +1,4 @@
-sed "s/PVCS_BASE_DIR/${PVCS_BASE_DIR}/g" kind.yaml > kind-effective.yaml
+sed "s|PVCS_BASE_DIR|${PVCS_BASE_DIR}|g" kind.yaml > kind-effective.yaml
 kind create cluster --config kind-effective.yaml
 kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/v1.1.0/config/crd/standard/gateway.networking.k8s.io_gatewayclasses.yaml
 kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/v1.1.0/config/crd/standard/gateway.networking.k8s.io_gateways.yaml
