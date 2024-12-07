e34cfa2971e50edc91ee1b6ed3aab6f974c4a35c
pedrocrc <pedrocrc@gmail.com>
2024-12-07 15:02:15 -0300

    Update Backstage deployment image to version 0.0.41 and switch to Elasticsearch search backend

--- backstage/backstage/packages/backend/src/index.ts
+++ backstage/backstage/packages/backend/src/index.ts
@@ -44,7 +44,8 @@ backend.add(import('@backstage/plugin-search-backend'));
 
 // search engine
 // See https://backstage.io/docs/features/search/search-engines
-backend.add(import('@backstage/plugin-search-backend-module-pg'));
+// backend.add(import('@backstage/plugin-search-backend-module-pg'));
+backend.add(import('@backstage/plugin-search-backend-module-elasticsearch'));
 
 // search collators
 backend.add(import('@backstage/plugin-search-backend-module-catalog'));
@@ -52,5 +53,4 @@ backend.add(import('@backstage/plugin-search-backend-module-techdocs'));
 
 // kubernetes
 backend.add(import('@backstage/plugin-kubernetes-backend'));
-backend.add(import('@backstage/plugin-search-backend-module-elasticsearch'));
 backend.start();
